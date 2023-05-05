f8ed906c652dad1a57feb6ff21078f57247ec1cd
林贤亮-01384950 <xianlianglin@sfmail.sf-express.com>
2023-05-05 17:35:03 +0800

    chang file name

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import { terser } from "rollup-plugin-terser"
 
 
 export default {
-  input: path.resolve(__dirname, 'lib/index.js'), // __dirname指的是当前文件所在文件夹的绝对路径。
+  input: path.resolve(__dirname, 'lib/index.js'),
 
   plugins: [
     terser({ compress: {} })
